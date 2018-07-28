328138b11e6cf7074995a674edb368b9ffeba179
서재원 <sbshw1@gmail.com>
2018-07-28 15:45:57 +0900

    simplify model

--- ENvironmentSet/src/model.js
+++ ENvironmentSet/src/model.js
@@ -36,60 +36,49 @@ export default class Model {
 
 export const addTodo = p.pipe(
   p.wrap,
-  p.partial(
-    flow,
-    ([desc, model]) => flow(
-      id => flow(
-        storage => flow(
-          item => flow(
-            register => register(id, item),
+  ([desc, model]) => flow(
+    id => flow(
+      storage => flow(
+        item => flow(
+          register => register(id, item),
           p.tie(storage, Map.prototype.set)),
-          Reflect.construct(TodoItem, p.wrap(id, desc))),
-        p.prop('storage', model)),
-      p.prop('uid', model))
+        Reflect.construct(TodoItem, p.wrap(id, desc))),
+      p.prop('storage', model)),
+    p.prop('uid', model)
   )
 );
 
 export const getTodo = p.pipe(
   p.wrap,
-  p.partial(
-    flow,
-    ([id, model]) => flow(
-      storage => flow(
-        getter => getter(id) || Reflect.construct(TodoItem),
+  ([id, model]) => flow(
+    storage => flow(
+      getter => getter(id) || Reflect.construct(TodoItem),
       p.tie(storage, Map.prototype.get)),
-    p.prop('storage', model))
+    p.prop('storage', model)
   )
 );
 
 export const removeTodo = p.pipe(
   p.wrap,
-  p.partial(
-    flow,
-    ([id, model]) => flow(
-      storage => flow(
-        remover => remover(id),
-        p.tie(storage, Map.prototype.delete)),
-      p.prop('storage', model))
+  ([id, model]) => flow(
+    storage => flow(
+      remover => remover(id),
+      p.tie(storage, Map.prototype.delete)),
+    p.prop('storage', model)
   )
 );
 
 export const finishTodo = p.pipe(
   p.wrap,
-  p.partial(
-    flow,
-    ([id, model]) => flow(
-      done,
-    getTodo(id, model))
+  ([id, model]) => flow(
+    done,
+    getTodo(id, model)
   )
 );
 
 export const beginTodo = p.pipe(
   p.wrap,
-  p.partial(
-    flow,
-    ([id, model]) => flow(
-      start,
-      getTodo(id, model))
-  )
+  ([id, model]) => flow(
+    start,
+    getTodo(id, model))
 );
