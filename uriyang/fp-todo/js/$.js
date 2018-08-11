70733c8e28ebe888569ba39756cfefde80f1e768
uriyang <girlstoryk@gmail.com>
2018-08-12 00:01:23 +0900

    todo check

--- uriyang/fp-todo/js/$.js
+++ uriyang/fp-todo/js/$.js
@@ -1,12 +1,31 @@
 !function() {
-  const $ = sel => document.querySelector(sel);
+  const baseSel = method => (sel, parent = document) => parent[method](sel);
+  const $ = baseSel('querySelector');
+  // $.find = (sel, parent) => $(sel, parent);
+  $.find = curry($);
+  $.all = baseSel('querySelectorAll');
+  $.findAll = curry($.all)
 
   $.el = html => {
-    const parent = document.createElement('div');
-    parent.innerHTML = html;
-    return parent;
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = html;
+    return wrapper;
   };
 
+  // $.append = function (parent, child) {
+  //   return arguments.length == 1 ? child => $append(parent, child) :
+  //   parent.appendChild(child);
+  // }  -> curry함수 이용
+  // 자동 형변환은 다른 타입이 들어올 경우가 문제가 되는데..
+  // == 값 비교, === 값 & 타입 비교
+  // length의 리턴 타입은 부호 없는 32비트 정수형
+  // 오히려 암묵적 형변환은 잘 쓰면 좋다.
+  // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/length
   $.append = curry((parent, child) => parent.appendChild(child));
+
+  $.on = (delegateTarget, eventName, f = sel, sel) => {
+    delegateTarget.addEventListener(eventName, f);
+  };
+
   window.$ = $;
 } ();
--- uriyang/fp-todo/js/function.js
+++ uriyang/fp-todo/js/function.js
@@ -1,80 +1,92 @@
-function log(v) {
-  console.log.apply(console, arguments);  // apply 아래에..
-  return v; // undefined를 찍지 않는다. PIPE 흐름에 자연스럽게~
-}
+!function() {
+  // curry
+  // function _curry(fn) {
+  //   return function(a, b) {
+  //     return arguments.length == 2 ? fn(a, b) : function(b) { return fn(a, b); };
+  //   }
+  // }
+  const curry = f => (...args) => args.length < 2 ? (...args2) => f(...args, ...args2) : f(...args);
 
-function _is_object(obj) {
-  return typeof obj == 'object' && !!obj;
-}
-
-function _keys(obj) {
-  return _is_object(obj) ? Object.keys(obj) : [];
-}
-
-function _each(list, iter) {
-  var keys = _keys(list);
-  for (var i = 0, len = keys.length; i < len; i++) {
-    iter(list[keys[i]], keys[i]);
+  function log(v) {
+    console.log.apply(console, arguments);  // apply 아래에..
+    return v; // undefined를 찍지 않는다. PIPE 흐름에 자연스럽게~
   }
-  return list;
-}
 
-// ## map ## //
-// es5 style
-function map(list, mapper) {
-  var new_list = [];
-  _each(list, function(val, key) {
-    new_list.push(mapper(val, key));
-  });
-  return new_list;
-}
+  function _is_object(obj) {
+    return typeof obj == 'object' && !!obj;
+  }
 
-// ## filter ##
-function _filter(list, predi) {
-  var new_list = [];
-  _each(list, function(val) {
-    if (predi(val)) new_list.push(val);
-  });
-  return new_list;
-}
+  function _keys(obj) {
+    return _is_object(obj) ? Object.keys(obj) : [];
+  }
 
-// reduce
-// function _reduce(list, iter, memo) {
-//   if (arguments.length == 2) {
-//     memo = list[0];
-//     list = _rest(list);
-//   }
-//   _each(list, function(val) {
-//     memo = iter(memo, val);
-//   });
-//   return memo;
-// }
-const reduce = (f, acc, coll) => {
-  for (const val of coll) acc = f(acc, val);
-  return acc;
-}
+  function _each(list, iter) {
+    var keys = _keys(list);
+    for (var i = 0, len = keys.length; i < len; i++) {
+      iter(list[keys[i]], keys[i]);
+    }
+    return list;
+  }
 
-// curry
-// function _curry(fn) {
-//   return function(a, b) {
-//     return arguments.length == 2 ? fn(a, b) : function(b) { return fn(a, b); };
-//   }
-// }
-const curry = f => (..._) => _.length < 2 ? (..._2) => f(..._, ..._2) : f(..._);
+  // ## map ## //
+  // es5 style
+  function map(list, mapper) {
+    var new_list = [];
+    _each(list, function(val, key) {
+      new_list.push(mapper(val, key));
+    });
+    return new_list;
+  }
 
-// go
-function _go(arg) {
-  var fns = _rest(arguments);
-  return _pipe.apply(null, fns)(arg); // apply https://blog.weirdx.io/post/3214
-}
+  // ## filter ##
+  function _filter(list, predi) {
+    var new_list = [];
+    _each(list, function(val) {
+      if (predi(val)) new_list.push(val);
+    });
+    return new_list;
+  }
 
-// pipe
-// function _pipe() {
-//   var fns = arguments;
-//   return function(arg) {
-//     return _reduce(fns, function(arg, fn) {
-//       return fn(arg);
-//     }, arg);
-//   }
-// }
-const pipe = (...fs) => arg => fs.reduce((arg, f) =>f(arg), arg);
+  // reduce
+  // function _reduce(list, iter, memo) {
+  //   if (arguments.length == 2) {
+  //     memo = list[0];
+  //     list = _rest(list);
+  //   }
+  //   _each(list, function(val) {
+  //     memo = iter(memo, val);
+  //   });
+  //   return memo;
+  // }
+  const reduce = curry((f, acc, coll) => {
+    for (const val of coll) acc = f(acc, val);
+    return acc;
+  })
+
+  const find = curry((f, coll) => {
+    for (var val of coll) if (f(val)) return val
+  })
+
+  // go
+  // function _go(arg) {
+  //   var fns = _rest(arguments);
+  //   return _pipe.apply(null, fns)(arg); // apply https://blog.weirdx.io/post/3214
+  // }
+
+  const go = (arg, ...fs) => reduce((arg, f) => f(arg), arg, fs);
+
+  // pipe
+  // function _pipe() {
+  //   var fns = arguments;
+  //   return function(arg) {
+  //     return _reduce(fns, function(arg, fn) {
+  //       return fn(arg);
+  //     }, arg);
+  //   }
+  // }
+  const pipe = (...fs) => arg => reduce((arg, f) => f(arg), arg, fs);
+
+  Object.assign(window, {
+    log, curry, pipe, go, reduce, find
+  })
+} ();
